8ccd6d03339158fbaf5263a618977ee08ec6219b
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-04-22 10:13:51 -0600

    POST new URL

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,3 +5,14 @@ export const getUrls = () => {
         return response.json()
       })
 }
+
+export const postUrls = (newUrl) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify(newUrl),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {return response.json()})
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
+import { postUrls } from '../../apiCalls';
 
 function App () {
   const [urls, setUrls] = useState([]);
@@ -10,16 +11,23 @@ function App () {
   useEffect(() => {
     getUrls()
     .then(data => {
-      console.log(data.urls)
+      // console.log(data.urls)
       setUrls(data.urls)
     })
   }, [])
+
+  const addUrl = (newUrl) => {
+    postUrls(newUrl)
+    .then(data => {
+      setUrls([...urls, data])
+    })
+  }
 
   return (
     <main className="App">
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm />
+        <UrlForm addUrl={addUrl}/>
       </header>
       <UrlContainer urls={urls}/>
     </main>
